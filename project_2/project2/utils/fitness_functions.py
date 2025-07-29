94c640fb0b3e351649fbcf2b5fd8b66f49602b48
Marcell Schuh <marcell@mntr.dev>
2025-07-29 17:52:11 +0200

    Add clamped fitness function

--- project_2/project2/utils/fitness_functions.py
+++ project_2/project2/utils/fitness_functions.py
@@ -133,4 +133,9 @@ def max_distance(
             # Use regular distance calculation
             total_distance += xy_displacement(current_robot_state, next_robot_state)
 
-    return total_distance
+    # Clamp the distance to the plane size, so that the fitness function is always between 0 and 1
+    if plane_size is not None:
+        return min(1.0, total_distance / (2 * plane_size))
+    else:
+        logging.warning("Plane size is not set, using regular distance calculation")
+        return total_distance
